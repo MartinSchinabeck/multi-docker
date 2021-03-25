560c5f83907fcd43283711796c793cc11af904b3
Martin.Schinabeck <martin.sedna.de>
2021-03-25 23:16:50 +0100

    typo in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Fib-Calculator</h1>
           <Link to="/">Home</Link>
-          <Link to "/">Learn React</Link>
+          <Link to="/">Learn React</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
